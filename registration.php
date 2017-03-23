2a6ed8ad4f38b2dd7c1bc974c47c71a97a29ccdc
Sergey <Serega.Vili@yandex.ru>
2017-03-23 18:34:51 +0300

    Add files via upload

--- registration.php
+++ registration.php
@@ -1,7 +1,5 @@
 <?php 
 
-    
-
 $errors = [];
 
 $lastName = $_POST['lastName'];
@@ -20,48 +18,57 @@ $pass= htmlspecialchars($pass);
 
 $login = mb_strtolower($login);
 
-if (preg_match ('/\s/', $lastName)){
-	$errors[] = 'Фамилия не должна содержать пробелов';
-}else{
-	if (preg_match ('/[0-9]/', $lastName)){
-		$errors[] = 'Фамилия не должна содержать цифр';
+if ($lastName!="") {
+	if (preg_match ('/\s/', $lastName)){
+		$errors[] = 'Фамилия не должна содержать пробелов';
 	}else{
-		if (preg_match ('/(^A-Za-z0-9)/', $lastName ) ){
-			$errors[] = 'Фамилия должна быть только из букв и без пробелов';
+		if (preg_match ('/[0-9]/', $lastName)){
+			$errors[] = 'Фамилия не должна содержать цифр';
+		}else{
+			if (!preg_match("~^[a-zа-я]+$~ui",$lastName) ){
+				$errors[] = 'Фамилия должна быть только из букв и без пробелов';
+			}
 		}
 	}
 }
 
-if (preg_match ('/\s/', $firstName)){
-	$errors[] = 'Имя не должно содержать пробелов';
-}else{
-	if (preg_match ('/[0-9]/', $firstName)){
-		$errors[] = 'Имя не должно содержать цифр';
+
+if ($firstName!="") {
+	if (preg_match ('/\s/', $firstName)){
+		$errors[] = 'Имя не должно содержать пробелов';
 	}else{
-		if (preg_match ('/(^A-Za-z0-9)/', $firstName ) ){
-			$errors[] = 'Имя должно быть только из букв и без пробелов';
+		if (preg_match ('/[0-9]/', $firstName)){
+			$errors[] = 'Имя не должно содержать цифр';
+		}else{
+			if (!preg_match("~^[a-zа-я]+$~ui",$firstName)){
+				$errors[] = 'Имя должно быть только из букв и без пробелов';
+			}
 		}
 	}
 }
 
-if (preg_match ('/\s/', $fatherName)){
-	$errors[] = 'Отчество не должно содержать пробелов';
-}else{
-	if (preg_match ('/[0-9]/', $fatherName)){
-		$errors[] = 'Отчество не должно содержать цифр';
+
+if ($fatherName!="") {
+	if (preg_match ('/\s/', $fatherName)){
+		$errors[] = 'Отчество не должно содержать пробелов';
 	}else{
-		if (preg_match ('/(^A-Za-z0-9)/', $fatherName ) ){
-			$errors[] = 'Отчество должно быть только из букв и без пробелов';
+		if (preg_match ('/[0-9]/', $fatherName)){
+			$errors[] = 'Отчество не должно содержать цифр';
+		}else{
+			if (!preg_match("~^[a-zа-я]+$~ui",$firstName)){
+				$errors[] = 'Отчество должно быть только из букв и без пробелов';
+			}
 		}
 	}
 }
+
 
 if (!preg_match ('/[a-z]+$/i', $login))
-$errors[] = 'Не валидный логин. Введите только латинские буквы в нижнем регистре.';
+	$errors[] = 'Не валидный логин. Введите только латинские буквы в нижнем регистре.';
 
 if($passNum!=""){
-if (!preg_match ('/^\d{10}$/', $passNum))
-$errors[] = 'Не валидный номер паспорта. Должны быть только 10 цифр.';
+	if (!preg_match ('/^\d{10}$/', $passNum))
+		$errors[] = 'Не валидный номер паспорта. Должны быть только 10 цифр.';
 }
 
 if (preg_match ('/\s{10,}/', $pass)){
@@ -70,26 +77,26 @@ if (preg_match ('/\s{10,}/', $pass)){
 
 
 if($_POST['dateBorn']!=""){
-if (!preg_match ('/^[0-9]{1,2}[.][0-9]{1,2}[.][0-9]{4}$/', $_POST['dateBorn'])){
-	$errors[] = 'Не валидная дата.';
-}else{
-	
-	$date=date('Y-m-d', strtotime($_POST['dateBorn']));
-	if ($date > date('Y-m-d')) {
-		$errors[] = 'Дата должна быть меньше текущей';
+	if (!preg_match ('/^[0-9]{1,2}[.][0-9]{1,2}[.][0-9]{4}$/', $_POST['dateBorn'])){
+		$errors[] = 'Не валидная дата.';
+	}else{
+		
+		$date=date('Y-m-d', strtotime($_POST['dateBorn']));
+		if ($date > date('Y-m-d')) {
+			$errors[] = 'Дата должна быть меньше текущей';
+		}
 	}
-}
 }
 
 
 if ($errors) {
-  
-  $content = '<div class="mistakes_head">При отправке формы произошли следующие ошибки:</div>';
-  foreach ($errors as $error) $content .='<div class="mistake"> <span class="warn">*</span>'.$error.'</div>';
+	
+	$content = '<div class="mistakes_head">При отправке формы произошли следующие ошибки:</div>';
+	foreach ($errors as $error) $content .='<div class="mistake"> <span class="warn">*</span>'.$error.'</div>';
 
-  $content.='<div class="mistakes_foot">Для продолжения регистрации исправьте ошибки.</div>';
-  echo $content ;
-  
+	$content.='<div class="mistakes_foot">Для продолжения регистрации исправьте ошибки.</div>';
+	echo $content ;
+	
 }else{
 	if (file_exists($_SERVER["DOCUMENT_ROOT"].'/users/'.$login.'.user')) // Если пользователь существует
 	{
@@ -107,7 +114,7 @@ if ($errors) {
 	fclose ($file);
 	//header ('Location: success.php'); 
 	echo '<div class="success">Вы успешно зарегистрировались!</div>';
-	}
+}
 
 }
 ?>
